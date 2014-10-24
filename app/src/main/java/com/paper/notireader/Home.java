3e2c6bcc26efe0f407722b6240375f3548276409
PapEr <zw.paper@gmail.com>
2014-10-24 20:31:20 +0800

    Friday leave

--- app/src/main/java/com/paper/notireader/Home.java
+++ app/src/main/java/com/paper/notireader/Home.java
@@ -1,6 +1,7 @@
 package com.paper.notireader;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,10 +9,15 @@ import android.view.MenuItem;
 
 public class Home extends Activity {
 
+    NotificationManager notificationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
     }
 
 
